d577c17970aedd7d73d6ba60a2af76e496389aed
kiira254 <nkamotho57@gmail.com>
2020-10-31 08:20:53 +0300

    add views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, redirect
+import datetime as dt
+from django.contrib.auth.decorators import login_required
+from .forms import SignupForm
+from django.contrib.auth.models import User
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
+def signup(request):
+    name = "Sign Up"
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            email = form.cleaned_data.get('email')
+            name = form.cleaned_data.get('username')
+            send_mail(
+            'Welcome to Mini-Insta.',
+            f'Hello {name},\n '
+            'Welcome to Mini-Insta.',
+            'nkamotho69@gmail.com',
+            [email],
+            fail_silently=False,
+            )
+        return redirect('post')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form, 'name': name})
