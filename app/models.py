d450456f1ff2f611f137bffac07fce5fe7a90113
kiira254 <nkamotho57@gmail.com>
2020-10-31 12:07:10 +0300

    add profile to the models

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Profile (models.Model):
+    username = models.CharField(max_length = 50,null=True)
+    user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'Profile')
+    bio = models.TextField(null=True)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
+    email = models.EmailField(max_length = 60)
+
+def __str__(self):
+        return self.user.username
