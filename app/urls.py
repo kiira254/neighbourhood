9f991940fb0d7a93fb497f4418f920d85592898e
kiira254 <nkamotho57@gmail.com>
2020-11-01 12:16:18 +0300

    add Location user and neighborhood to admin

--- app/urls.py
+++ app/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url,include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path, re_path
 
 urlpatterns=[
     url(r'^$',views.home,name='home'),
-    
+    re_path('new_profile/(?P<username>\w{0,50})',views.new_profile,name = 'new_profile'),
     
 ]
 
